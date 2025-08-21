265b410ef57ffc8a697a03e9dcdf7e0f93bbe29e
Imaadh-Rushdee <rushdeeimaadh@gmail.com>
2025-08-21 15:18:48 +0530

    Firebase Credintials Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,6 +35,8 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
